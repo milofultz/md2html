65ba005c22fc13d6433f12c413f757f332e93d8e
Milo Fultz <milopfultz@gmail.com>
2021-05-06 14:37:54 -0700

    Iterate through all files in structures

--- md2html.py
+++ md2html.py
@@ -5,9 +5,23 @@ from markdown_parser.markdown_parser import MarkdownParser
 from templater.templater import Templater
 from split_fm_md import split_fm_md
 
+templates = Templater()
 
-def main():
+def main(files_dir: str, output_dir: str):
     # load all structures into template dict under _structures
+
+    structures_dir = os.path.join(files_dir, '_structures')
+    # if dir doesn't exist
+    if not os.path.isdir(structures_dir):
+        sys.exit('Input path does not exist or is not a directory.')
+    # for each file in subdirectory '_structures'
+    for file in os.listdir(structures_dir):
+        # with open the file as f
+        with open(os.path.join(structures_dir, file), 'r') as f:
+            # get the file contents and trim them
+            print(f.read())
+            # load contents into templates: {structures: {filename (no ext): ...}}
+
     # load all modules into template dict under _modules
     # for each page in pages
         # split front matter and markdown
@@ -24,6 +38,8 @@ if __name__ == '__main__':
         sys.exit('Please provide an input folder.')
     # set input_fp to arg 1
     # set output_fp to arg 2 if len(sys.argv) == 3 else arg 1
+    _, in_fp, out_fp = sys.argv
 
     # build the site
+    main(in_fp, out_fp)
     # success message
