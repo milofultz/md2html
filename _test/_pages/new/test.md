93407abd24b73869b3a4f14085df14aca8f03b66
Milo Fultz <milopfultz@gmail.com>
2021-05-06 19:24:14 -0700

    Add nested folder support and parse modules

---
+++ _test/_pages/new/test.md
@@ -0,0 +1,10 @@
+---
+title: Nested file WHOA
+description: OH YEAH
+structure: page
+---
+
+This would be cool if it *worked*.
+
+    The void
+    Wow
--- md2html.py
+++ md2html.py
@@ -10,38 +10,73 @@ md_parser = MarkdownParser()
 
 
 def main(files_dir: str, output_dir: str):
-    print('\nSite build start...\n')
+    print('\nSite build start...')
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
 
-    load_templates(os.path.join(files_dir, '_structures'), 'structures')
-    load_templates(os.path.join(files_dir, '_modules'), 'modules')
+    pages_exist = False
 
-    pages_dir = os.path.join(files_dir, '_pages')
-    pages = os.listdir(pages_dir)
-    for page in pages:
-        with open(os.path.join(pages_dir, page), 'r') as f:
-            raw_page = f.read()
-        front_matter, markdown = split_fm_md.split_page(raw_page)
-        templates.add_templates({'page': front_matter})
-        parsed_markdown = md_parser.parse(markdown)
-        templates.add_templates({'page': {'_html': parsed_markdown}})
-        finished_page = templates.fill_structure(front_matter['structure'])
-        page_name = page.rsplit('.', 1)[0]
-        with open(os.path.join(output_dir, f'{page_name}.html'), 'w') as f:
-            f.write(finished_page)
-        print(f'''{os.path.join(files_dir, "_pages", page)}  ->  {os.path.join(output_dir, page_name + ".html")}''')
+    for folder in os.listdir(files_dir):
+        if not os.path.isdir(os.path.join(files_dir, folder)):
+            continue
+        # means it is private or not for this step
+        if folder[0:2] == '__' or folder[0] != '_':
+            continue
+        elif folder == '_pages':
+            pages_exist = True
+        elif folder == '_modules':
+            load_templates(os.path.join(files_dir, folder), folder[1:], parsed=False)
+        else:
+            load_templates(os.path.join(files_dir, folder), folder[1:])
+
+    if not pages_exist:
+        sys.exit('No \'_pages\' folder was found.')
+    # recursively go through each file and folder and build pages
+    render_pages(os.path.join(files_dir, '_pages'), output_dir)
+
     print('\nSite build complete')
 
 
-def load_templates(dir_name: str, template_group: str):
+def load_templates(dir_name: str, template_group: str, parsed=True):
     if not os.path.isdir(dir_name):
         sys.exit('Input path does not exist or is not a directory.')
     for file in os.listdir(dir_name):
         with open(os.path.join(dir_name, file), 'r') as f:
             template_name = file.rsplit('.', 1)[0]
             template = f.read().strip()
+            if not parsed:
+                template = md_parser.parse(template)
             templates.add_templates({template_group: {template_name: template}})
+
+
+def render_pages(folder: str, output: str):
+    # for each folder including root
+    for subfolder, _, pages in os.walk(folder):
+        subfolder = subfolder[len(folder) + 1:]
+        print(f'''\nRendering pages in {"root" if subfolder == '' else f"'{subfolder}'"} folder...''')
+
+        for page in pages:
+            with open(os.path.join(folder, subfolder, page), 'r') as f:
+                raw_page = f.read()
+            front_matter, markdown = split_fm_md.split_page(raw_page)
+            parsed_markdown = md_parser.parse(markdown)
+            templates.add_templates({'page': {**front_matter, '_html': parsed_markdown}})
+            finished_page = templates.fill_structure(front_matter['structure'])
+
+            page_name = page.rsplit('.', 1)[0]
+            output_folder = os.path.join(output, subfolder)
+            if subfolder == '':
+                output_folder = os.path.join(output)
+            output_path = os.path.join(output_folder, f'{page_name}.html')
+
+            try:
+                os.mkdir(output_folder)
+            except FileExistsError:
+                pass
+
+            with open(output_path, 'w') as f:
+                f.write(finished_page)
+            print(f'''{os.path.join(subfolder, "_pages", page)}  ->  {output_path}''')
 
 
 if __name__ == '__main__':
